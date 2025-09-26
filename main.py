1e04614bd0d19bcff369863e75ae9c3d6e97c164
syswcq-xhs <481124168@qq.com>
2025-09-26 20:11:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import sys
 try:
-    year = int(input("请输入年份："))
-    if (year % 4 == 0 和 year % 100!=0) 或year%400==0:
+    year = int(sys.argv[1])
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
         print(f"{year}年是闰年")
     else:
         print(f"{year}年不是闰年")
-except ValueError：
-    print("输入错误")
+except IndexError:
+    try:
+        year = int(input("请输入年份："))
+        if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+            print(f"{year}年是闰年")
+        else:
+            print(f"{year}年不是闰年")
+    except ValueError:
+        print("输入错误")
